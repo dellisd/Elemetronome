ed1da94b85557c9f9cef4cb1b8ecda2c433f99cf
derek <derekellis170@yahoo.ca>
2017-05-24 21:30:18 -0400

    TAG

--- app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
+++ app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity";
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

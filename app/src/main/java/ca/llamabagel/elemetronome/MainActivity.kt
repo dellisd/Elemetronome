a719117be966288a1b38232ad6aff68159d828ed
derek <derekellis170@yahoo.ca>
2017-05-24 21:25:39 -0400

    Kotlin android extensions import

--- app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
+++ app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
@@ -3,6 +3,8 @@ package ca.llamabagel.elemetronome
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

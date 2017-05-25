9a28be4becb57dde7fd6f5a19ee0b0de79c366b8
derek <derekellis170@yahoo.ca>
2017-05-24 22:36:08 -0400

    It runs, at least

--- app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
+++ app/src/main/java/ca/llamabagel/elemetronome/MainActivity.kt
@@ -1,7 +1,9 @@
 package ca.llamabagel.elemetronome
 
+import android.media.AudioManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,5 +15,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Changes the volume control stream so that the volume buttons will work properly
+        volumeControlStream = AudioManager.STREAM_MUSIC
+
+        viewPager.adapter = PagerAdapter(supportFragmentManager)
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                val ids = listOf(R.id.navigation_metronome, R.id.navigation_tuner, R.id.navigation_tempo)
+                navigation.selectedItemId = ids[position]
+            }
+
+            override fun onPageSelected(position: Int) {}
+        })
+
+        navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_metronome -> viewPager.currentItem = 0
+                R.id.navigation_tuner -> viewPager.currentItem = 1
+                R.id.navigation_tempo -> viewPager.currentItem = 2
+            }
+
+            true
+        }
     }
 }
---
+++ app/src/main/java/ca/llamabagel/elemetronome/PagerAdapter.kt
@@ -0,0 +1,21 @@
+package ca.llamabagel.elemetronome
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+/**
+ * Created by derek on 5/24/2017.
+ * General ViewPager adapter for use by the app for the 3 main screens
+ */
+class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> MetronomeFragment.newInstance("M1")
+            1 -> MetronomeFragment.newInstance("M2")
+            else -> MetronomeFragment.newInstance("M3")
+        }
+    }
+
+    override fun getCount(): Int = 3
+}
